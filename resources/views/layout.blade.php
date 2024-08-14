e9e7bec65b2205343113da3f3e366b457078a16c
Heeran Kim <heerankim3@gmail.com>
2024-08-14 10:43:58 +1000

    Blade Components, Components Attributes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -29,7 +29,7 @@
     <body class="mb-48">
         <nav class="flex justify-between items-center mb-4">
             <a href="/"
-                ><img class="w-24" src="images/logo.png" alt="" class="logo"
+                ><img class="w-24" src="{{asset('images/logo.png')}}" alt="" class="logo"
             /></a>
             <ul class="flex space-x-6 mr-6 text-lg">
                 <li>
@@ -50,6 +50,7 @@
             {{-- VIEW OUTPUT --}}
             @yield('content')
         </main>
+
         <footer
             class="fixed bottom-0 left-0 w-full flex items-center justify-start font-bold bg-laravel text-white h-24 mt-24 opacity-90 md:justify-center"
         >
--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -4,13 +4,11 @@
 @include('partials/_search')
 
 <a href="/" class="inline-block text-black ml-4 mb-4">
-    <i class="fa-solid fa-arrow-left"></i> Back
-</a>
+    <i class="fa-solid fa-arrow-left"></i> Back </a>
+
 <div class="mx-4">
-    <div class="bg-gray-50 border border-gray-200 p-10 rounded">
-        <div
-            class="flex flex-col items-center justify-center text-center"
-        >
+    <x-card class="!p-10">
+        <div class="flex flex-col items-center justify-center text-center">
             <img
                 class="w-48 mr-6 mb-6"
                 src="{{asset('images/no-image.png')}}"
@@ -69,7 +67,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    </x-card>
 </div>
-
 @endsection
