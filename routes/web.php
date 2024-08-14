b3df7f869659474318e6e7cc8db6a7f8a1b8fef1
Heeran Kim <heerankim3@gmail.com>
2024-08-14 11:05:34 +1000

    resource method naming

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Listing;
+use App\Http\Controllers\ListingController;
+
+// All Listings (before making controller)
+// Route::get('/', function () {
+//     return view('listings', [
+//         'listings' => Listing::all()
+//     ]);
+// });
 
 // All Listings
-Route::get('/', function () {
-    return view('listings', [
-        'heading' => 'Latest Listings',
-        'listings' => Listing::all()
-    ]);
-});
+Route::get('/', [ListingController::class, 'index']);
 
-// Single Listing (Old)
+
+// Single Listing (before binding model)
 // Route::get('/listings/{id}', function ($id) {
 //     $listing = Listing::find($id);
 
@@ -24,9 +27,13 @@ Route::get('/', function () {
 //     }
 // });
 
+// Single Listing (before making controller)
+// Route::get('/listings/{listing}', function (Listing $listing) {
+//     return view('listing', [
+//         'listing' => $listing
+//     ]);
+// });
+
 // Single Listing
-Route::get('/listings/{listing}', function (Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-});
+Route::get('/listings/{listing}',
+[ListingController::class, 'show']);
